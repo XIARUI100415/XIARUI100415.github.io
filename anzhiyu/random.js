f896bcc4f5ba1953388babab60d3afe9ceb3fded
XIARUI100415 <1948083823@qq.com>
2024-09-01 11:54:42 +0800

    Site updated: 2024-09-01 11:54:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/15/hello-world/"];function toRandomPost(){
+var posts=["2024/08/19/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
